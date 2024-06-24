452ebbc4685a3a6cca57fd9f4e500ae1af7666fa
maaaaaaaaad <integral5137@gmail.com>
2024-06-24 15:37:56 +0900

    e

--- src/main/java/com/root/mad/MadApplication.java
+++ src/main/java/com/root/mad/MadApplication.java
@@ -1,13 +1,16 @@
 package com.root.mad;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MadApplication.class, args);
-	}
+    public static void main(String[] args) {
+        final Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.out.println(entry.getKey() + "=" + entry.getValue()));
+        SpringApplication.run(MadApplication.class, args);
+    }
 
 }
